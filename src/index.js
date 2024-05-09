b429c29d94d2627242da5f81378b6e76a1883ad1
mangat-ram <se.mangat.ram@gmail.com>
2024-05-09 21:46:34 +0530

    Added Index Logic.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Index Js Loaded.");
+import dotenv from "dotenv";
+import { connectDatabase } from "./database/connectDatabase.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env"
+})
+
+connectDatabase()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is Listening on Port ${process.env.PORT}`);
+  })
+})
+.catch((error) => {
+  console.log("Mongo DB Connection Failed!!! ",error);
+})
