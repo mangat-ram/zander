3ebfedd6def911fe062448545927639d1e7e9985
mangat-ram <se.mangat.ram@gmail.com>
2024-05-10 07:56:43 +0530

    Added Utility Class API Error

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+// Error Handling Class to give Right Error to User Or FrontEnd Engineer
+
+class ApiError extends Error{
+  constructor(
+    statusCode,
+    message = "Something went wrong!!!",
+    errors = [],
+    stack = ""
+  ){
+    super(message)
+    this.statusCode = statusCode,
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
