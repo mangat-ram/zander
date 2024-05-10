3d8af5907bf001a0cf12d700850d21e5352ed3b7
mangat-ram <se.mangat.ram@gmail.com>
2024-05-10 07:24:46 +0530

    Currently Working on User model.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,38 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    trim:true,
+    index:true
+  },
+  name: {
+    type:String,
+    required:true,
+    trim:true,
+    index:true
+  },
+  email: {
+    type:String,
+    required: true,
+    unique:true,
+    trim:true
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."]
+  },
+  isVerified: {
+    
+  },
+  verifyCode: {
+
+  }
+},{timestamps:true})
