fa9f3287f9be38fef5b636c6a6496c72d88312c4
mangat-ram <se.mangat.ram@gmail.com>
2024-05-11 13:24:44 +0530

    Added Regiter User method.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -38,10 +38,58 @@ const checkUniqueUser = asyncHandler( async(req,res) => {
     )
 })
 
+const registerUser = asyncHandler(async(req,res) =>{
+  const { username, name, email, password } = req.body;
+
+  if([username, name, email, password].some((field) => {
+    return field?.trim() === ""
+  })){
+    throw new ApiError (400,"all fields are required.")
+  }
+
+  const existedUser = await User.findOne({
+    $or : [{username},{email},{verifyCode:true}]
+  })
+
+  if(existedUser){
+    throw new ApiError(409,"user with email or username already exists.")
+  }
+
+  const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+  const verifyCodeExpiry = new Date();
+  verifyCodeExpiry.setHours(verifyCodeExpiry.getHours() + 1);
+
+  const user = await User.create({
+    username:username.toLowerCase(),
+    name,
+    email,
+    password,
+    verifyCode,
+    verifyCodeExpiry,
+    isVerified:false
+  })
+
+  const createdUser = await User.findById(user._id).select(
+    "-password refreshToken -verifyCode -verifyCodeExpiry"
+  )
+
+  if(!createdUser){
+    throw new ApiError(500,"something went wrong while registering user.")
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(200,createdUser,"user regitered successfully.")
+    )
+})
+
+
 
 
 
 export {
   generateAccessAndRefreshToken,
-  checkUniqueUser
+  checkUniqueUser,
+  registerUser
 }
