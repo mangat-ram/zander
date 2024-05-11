c1e7753a3b87c80fa8c6fde716ae1cd2911cf56b
mangat-ram <se.mangat.ram@gmail.com>
2024-05-11 13:10:17 +0530

    Added Check unique user Method.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,3 +19,29 @@ const generateAccessAndRefreshToken = async(userId) => {
     throw new ApiError(500,"Something went wrong while genrating access and refresh token.!!!")
   }
 }
+
+const checkUniqueUser = asyncHandler( async(req,res) => {
+  const { username } = req.params;
+  const user = await User.findOne({username});
+  if(user){
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(200,{},"username already exists!")
+      )
+  }
+  
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(200,{},"username is available")
+    )
+})
+
+
+
+
+export {
+  generateAccessAndRefreshToken,
+  checkUniqueUser
+}
