d9eeb60b9e76d8c005f852ece603f0a731f3b90b
mangat-ram <se.mangat.ram@gmail.com>
2024-05-11 13:04:15 +0530

    Added Generate Access And Refresh Token Control Method.

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,21 @@
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+
+const generateAccessAndRefreshToken = async(userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave : false })
+
+    return { accessToken, refreshToken }
+  } catch (error) {
+    throw new ApiError(500,"Something went wrong while genrating access and refresh token.!!!")
+  }
+}
