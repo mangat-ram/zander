e578e1bf0429c7e546298b953f998aaeb96e73e4
mangat-ram <se.mangat.ram@gmail.com>
2024-05-13 22:13:57 +0530

    added refresh token API.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -214,6 +214,50 @@ const logoutUser = asyncHandler(async (req, res) => {
     )
 })
 
+const refreshToken = asyncHandler(async(req,res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+  if(!incomingRefreshToken){
+    throw new ApiError(401,"Unauthorized request!")
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+
+    const user = await User.findById(decodedToken?._id)
+    if(!user){
+      throw new ApiError(401,"invalid refresh token!!!")
+    }
+
+    if(incomingRefreshToken !== user?.refreshToken){
+      throw new ApiError(401,"refresh token is expired!!")
+    }
+
+    const options = {
+      httpOnly:true,
+      secure:true
+    }
+
+    const { accessToken, newRefreshToken } = await generateAccessAndRefreshToken(user._id);
+
+    return res
+      .status(200)
+      .cookie("accessToken",accessToken,options)
+      .cookie("refreshToken", newRefreshToken,options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken,newRefreshToken },
+          "access token refreshed successfully."
+        )
+      )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "invalid refresh token catched!!")
+  }
+})
+
 export {
   generateAccessAndRefreshToken,
   checkUniqueUser,
